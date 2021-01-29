bd68285f4366a87e6c368ec1e4fcb31285926b33
Madshtik <jann_cosio@yahoo.com>
2021-01-29 19:04:49 +0400

    Re-did the irst person mechanics and made it into a third person game in accordance with the team's decision:
    Added Cinemachine Package and applied it to the player
    
    PlayerMovement script has been changed - added new logic to incorporate third person controls and removed all first person logic; kept the jump logic, but moved it to a method called 'Jump,' that takes in a jump height float variable
    
    Removed the MouseLook script as it was deemed redundant and messed with the third person controls

--- Project Mafia/Assets/Scripts/PlayerMovement.cs
+++ Project Mafia/Assets/Scripts/PlayerMovement.cs
@@ -6,35 +6,48 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
+    public Transform myCamera;
     public Transform groundCheck;
 
     public LayerMask groundMask;
 
     [Header("Public Variables")]
-    public float mySpeed;
-    public float jumpHeight;
     public float gravity;
     public float groundDistance;
+    public float jumpHeight;
+    public float mySpeed;
+    public float turnSmoothTime;
 
     Vector3 velocity;
 
     bool isGrounded;
 
+    float turnSmoothVelocity;
     float x;
     float z;
 
     void Update()
     {
-        x = Input.GetAxis("Horizontal");
-        z = Input.GetAxis("Vertical");
+        //handles moving
+        x = Input.GetAxisRaw("Horizontal");
+        z = Input.GetAxisRaw("Vertical");
 
-        Vector3 myMovement = transform.right * x + transform.forward * z;
+        Vector3 myMovement = new Vector3(x, 0, z).normalized;
 
-        controller.Move(myMovement * mySpeed * Time.deltaTime);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (myMovement.magnitude >= 0.1f)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+            float targetAngle = Mathf.Atan2(myMovement.x, myMovement.z) * Mathf.Rad2Deg + myCamera.eulerAngles.y; //uses formula - theta = tan2(x/z) for unity obj rotation & myCamera used to rotate obj to cam rotation
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime); //smoothing rotation of character
+
+            transform.rotation = Quaternion.Euler(0, angle, 0);//actual rotation
+
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward; //move in direction of the camera
+            controller.Move(moveDirection.normalized * mySpeed * Time.deltaTime);
+        }
+
+        if (Input.GetButtonDown("Jump")) //handles jumping
+        {
+            Jump(jumpHeight);
         }
 
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
@@ -44,8 +57,18 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = -2;
         }
 
-        controller.Move(velocity * Time.deltaTime); //handles moving
+        controller.Move(velocity * Time.deltaTime);
 
         velocity.y += gravity * Time.deltaTime; //handles falling
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Jump(float thisJumpHeight)
+    {
+        if (isGrounded) //handles jumping
+        {
+            velocity.y = Mathf.Sqrt(thisJumpHeight * -2 * gravity); //using formula v^2 = h x 2g
+        }
     }
 }
