a29247491b3ac27f92ad0da67fe2bd519f982041
Madshtik <jann_cosio@yahoo.com>
2021-01-30 00:12:55 +0400

    Added a new script called ChefController (set  up needs a chef, at least 16 empty game objects for points of rotation to be referenced in Rotation Points List, and an empty gameObject to be referenced for the rotObj Transform - contact me for any questions on set up)
    Renamed TempFolder to Don't Delete
    Added subfolder into Don't Delete folder for prefabs
    Reorganized the scripts folder by adding subfolders for the player and the chef
    
    Added new scene to test the chef rotation in

---
+++ Project Mafia/Assets/Scripts/ChefAI/FieldOfView.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public List<Transform> visibleTargets = new List<Transform>();
+
+    [Header("Public Structs")]
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    [Header("Public Variables")]
+    public float searchDelay;
+    public float viewRadius;
+    [Range(0, 360)]
+    public float viewAngle;
+
+    void Start()
+    {
+        StartCoroutine("FindTargetWithDelay", searchDelay); 
+    }
+
+    IEnumerator FindTargetWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTarget();
+        }
+    } 
+
+    void FindVisibleTarget()
+    {
+        visibleTargets.Clear();
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < viewAngle/2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstacleMask))
+                {
+                    Debug.Log("Target Found");
+                    visibleTargets.Add(target);
+                }
+            }
+        }
+    }
+
+    public Vector3 DirFromAngle(float angleInDeg, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDeg += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDeg * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDeg * Mathf.Deg2Rad));
+    }
+}
