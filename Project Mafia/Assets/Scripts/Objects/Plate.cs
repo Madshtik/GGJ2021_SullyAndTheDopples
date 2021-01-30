dc320216038702a7d466aeb06931f4c8084aa6f2
Madshtik <jann_cosio@yahoo.com>
2021-01-30 22:10:08 +0400

    Enabled the win timeline to work

--- Project Mafia/Assets/Scripts/Objects/Plate.cs
+++ Project Mafia/Assets/Scripts/Objects/Plate.cs
@@ -1,15 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 
 public class Plate : MonoBehaviour
 {
+    public PlayableDirector director;
+
+    bool hasStarted = false;
+
+    void Update()
+    {
+        if (director.state != PlayState.Playing && hasStarted)
+        {
+            SceneManager.LoadScene(4);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene(4);
+            hasStarted = true;
+            director.Play();
         }
     }
 }
